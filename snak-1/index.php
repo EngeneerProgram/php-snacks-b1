12fab367b5a29d25764c8a63b3f367d43dcde421
Rosario Garufi <saro.garufi1@gmail.com>
2022-05-20 12:42:01 +0200

    Initial commit

---
+++ snak-1/index.php
@@ -0,0 +1,33 @@
+<!-- Creiamo un array contenente le partite di basket di un’ipotetica 
+tappa del calendario. Ogni array avrà una squadra di casa e una squadra 
+ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. 
+Stampiamo a schermo tutte le 
+partite con questo schema. Olimpia Milano - Cantù | 55-60  -->
+
+<?php
+$play_basket = [
+    [
+        'squadra_casa' => "Milano",
+        'squadra_fuori_casa' => "Roma",
+        "punti" => 58-70,
+    ],
+
+    [
+        'squadra_casa' => "Catania",
+        'squadra_fuori_casa' => "Messina",
+        "punti" => 50-15,
+    ],
+
+    [
+        'squadra_casa' => "Firenza",
+        'squadra_fuori_casa' => "Bari",
+        "punti" => 20-70,
+    ],
+
+]
+
+for($i = 0; i<$play_basket; i++){
+    
+}
+
+?>
